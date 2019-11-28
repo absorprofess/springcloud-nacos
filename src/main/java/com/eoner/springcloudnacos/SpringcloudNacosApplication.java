e405c3cc599eb0dc945db60c97ad7ef80a0bd6c4
absorprofess <absorprofess@outlook.com>
2019-11-28 14:18:46 +0800

    Initial commit

---
+++ src/main/java/com/eoner/springcloudnacos/SpringcloudNacosApplication.java
@@ -0,0 +1,13 @@
+package com.eoner.springcloudnacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudNacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudNacosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eoner/springcloudnacos/SpringcloudNacosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eoner.springcloudnacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudNacosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
